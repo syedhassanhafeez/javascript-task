3cd185a0e8cc72795ded744ba587175e3e043386
Syed Hassan <syedhassanbinhafeez@gmail.com>
2020-06-13 03:05:05 -1200

    Commit Updated

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ alert("Happy Coding!\nPrevent this page for creating additional dialogs.")
 console.log("Hello...I can run JS through my web browser's console")
 // task 6
 alert("Use of alert")
+
 // Chapter 2
 // task 1
 var username;
@@ -45,6 +46,7 @@ document.write("Yah! I can write HTML content through Javascript");
 // task 9
 alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
 document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
 // Chapter 3
 // task 1
 var age = "15";
@@ -52,7 +54,7 @@ alert("I am " + age + " years old");
 // task 2
 var visits;
 visits = parseInt(visits) + 1;
-document.write("You have visited this site "+visits+" times")
+document.write("You have visited this site " + visits + " times")
 // task 3
 var birthYear = 2000;
 document.write("My birth year is " + birthYear + "\nDatatype of my declared variable is number")
@@ -61,83 +63,468 @@ var Visitorsname = "John Doe";
 var Producttitle = "T-shirt";
 var Quantity = "5";
 document.write(Visitorsname + " ordered " + Quantity + Producttitle + "(s) on XYZ Clothing store");
+
 // Chapter 4
 // task 1
 var name1, name2, name3;
 // task 2
-var name1, name2, myName_3,name$4,name$5;
+var name1, name2, myName_3, name$4, name$5;
 // task 3
 document.write("<h3>Rules for naming JS variables</h3>")
-document.write("Variable names can only contain letters, numbers, dollar signs, and underscores.") 
+document.write("Variable names can only contain letters, numbers, dollar signs, and underscores.")
 document.write(" Variables must begin with a letter, $ . For example $name, _name or name ")
 document.write("Variable names are case sensitive")
 document.write(" Variable names should not be JS keywords")
+
 // Chapter 5
 // task 1
 var value1 = 3;
 var value2 = 5;
 var addvalue = value1 + value2;
-document.write("Sum of "+value1+" and "+value2+" is "+ addvalue)
+document.write("Sum of " + value1 + " and " + value2 + " is " + addvalue)
 // task 2
 var addvalue = value1 + value2;
-document.write("Sum of "+value1+" and "+value2+" is "+ addvalue)
+document.write("Sum of " + value1 + " and " + value2 + " is " + addvalue)
 var addvalue = value1 * value2;
-document.write("Sum of "+value1+" and "+value2+" is "+ addvalue)
+document.write("Sum of " + value1 + " and " + value2 + " is " + addvalue)
 var addvalue = value1 / value2;
-document.write("Sum of "+value1+" and "+value2+" is "+ addvalue)
+document.write("Sum of " + value1 + " and " + value2 + " is " + addvalue)
 var addvalue = value1 % value2;
-document.write("Sum of "+value1+" and "+value2+" is "+ addvalue)
+document.write("Sum of " + value1 + " and " + value2 + " is " + addvalue)
 // task 3
 var value;
 value = 5;
-document.write("Initial value:"+value)
+document.write("Initial value:" + value)
 var value1 = value + 1;
-document.write("Value after increment is: "+value1)
+document.write("Value after increment is: " + value1)
 var value2 = value1 + 7;
-document.write("Value after addition is: "+value2)
+document.write("Value after addition is: " + value2)
 var value3 = value2 - 1;
-document.write("Value after decrement is: "+value3)
+document.write("Value after decrement is: " + value3)
 var value4 = value3 % 3;
-document.write("The remainder is : "+value4)
+document.write("The remainder is : " + value4)
 // task 4
 var ticket = parseInt("600PKR");
 var ticket1 = 5;
 var totalCost = ticket * ticket1;
-document.write("Total cost to buy "+ticket1+" tickets to a movie is "+totalCost) 
+document.write("Total cost to buy " + ticket1 + " tickets to a movie is " + totalCost)
 // task 5
 var table;
 document.write("Table of 4<br /> 4 * 1 = 4<br /> 4 * 2 = 8<br /> 4 * 3 = 12<br /> 4 * 4 = 16<br /> 4 * 5 = 20<br /> 4 * 6 = 24<br />4 * 7 = 28<br /> 4 * 8 = 32<br />  4 * 9 = 36<br />  4 * 10 = 40<br />")
 // task 6
 var Celcius = 25;
-var Fahrenheit = (Celcius * 9/5) + 32 
-document.write(Celcius + " is "+Fahrenheit);
-var Fahrenheit1 = 70; 
-var Celcius1 = (Fahrenheit1 - 32) * (5/9);
+var Fahrenheit = (Celcius * 9 / 5) + 32
+document.write(Celcius + " is " + Fahrenheit);
+var Fahrenheit1 = 70;
+var Celcius1 = (Fahrenheit1 - 32) * (5 / 9);
 document.write(Fahrenheit1 + " is " + Celcius1);
 // task 7
 var priceofitem1 = 650;
 var priceofitem2 = 100;
 var orderedquantityofitem1 = 3;
 var orderedquantityofitem2 = 7;
-var shippingcharges = 100; 
-var totalCost = ((priceofitem1 * orderedquantityofitem1)+(priceofitem2 * orderedquantityofitem2)+shippingcharges)
-document.write("Price of item 1 is "+priceofitem1+"<br />Quantity of item 1 is "+orderedquantityofitem1+"<br />Price of item 2 is"+priceofitem2+"<br />Quantity of item 2 is "+orderedquantityofitem2+"<br />Shipping Charges "+shippingcharges)
-document.write("<br />Total cost of your order is "+totalCost)
+var shippingcharges = 100;
+var totalCost = ((priceofitem1 * orderedquantityofitem1) + (priceofitem2 * orderedquantityofitem2) + shippingcharges)
+document.write("Price of item 1 is " + priceofitem1 + "<br />Quantity of item 1 is " + orderedquantityofitem1 + "<br />Price of item 2 is" + priceofitem2 + "<br />Quantity of item 2 is " + orderedquantityofitem2 + "<br />Shipping Charges " + shippingcharges)
+document.write("<br />Total cost of your order is " + totalCost)
 // task 8
 var totalMarks = 980;
 var marksObtained = 804;
-var percentage = ((marksObtained/totalMarks)*100)
-document.write("<h2>Marks Sheet</h2><br /><br />Total marks:"+totalMarks+"<br />Marks obtained: "+marksObtained+"<br />Percentage: "+percentage)
+var percentage = ((marksObtained / totalMarks) * 100)
+document.write("<h2>Marks Sheet</h2><br /><br />Total marks:" + totalMarks + "<br />Marks obtained: " + marksObtained + "<br />Percentage: " + percentage)
 // task 9
 var usDollars = 104.80;
 var saudiRiyals = 28;
-var totalCurrency = ((10 * usDollars)+(25 * saudiRiyals))
-document.write("<h2>Currency in PKR </h2><br />Total Currency in PKR: "+totalCurrency)
+var totalCurrency = ((10 * usDollars) + (25 * saudiRiyals))
+document.write("<h2>Currency in PKR </h2><br />Total Currency in PKR: " + totalCurrency)
 // task 10
 var mathExpression = (((2 + 5) * 10) / 2)
-document.write(mathExpression) 
+document.write(mathExpression)
 // task 11
 var currentYear = 2016;
 var birthYear = 1992;
 var yourAge = 24;
-document.write("<h2>Age Calculator</h2><br />Current Year: "+currentYear+"<br />Birth Year: "+birthYear+"<br />Your Age: "+yourAge)
+document.write("<h2>Age Calculator</h2><br />Current Year: " + currentYear + "<br />Birth Year: " + birthYear + "<br />Your Age: " + yourAge)
+
+// Chapter 6-9
+// task 1
+var a = 10;
+document.write("<br>The value of a is: " + a)
+document.write("<br>.............................")
+var a = ++a;
+document.write("<br>The value of ++a is: " + a)
+document.write("<br>Now the value of a is: " + a)
+document.write("<br>The value of a++ is: " + a)
+var a = a + 1;
+document.write("<br>Now the value of a is: " + a);
+var a = --a;
+document.write("<br>The value of --a is: " + a)
+document.write("<br>Now the value of a is: " + a)
+document.write("<br>The value of a-- is: " + a)
+var a = a - 1;
+document.write("<br>Now the value of a is: " + a)
+// task 2
+var a = 2, b = 1;
+document.write("<br>a is: " + a)
+document.write("<br>b is: " + b)
+var result = --a - --b + ++b + b--;
+document.write("<br>result is: " + result)
+// task 3
+var name = prompt("Enter a name")
+document.write("Good Morning " + name)
+// task 4;
+var mul = +prompt("Enter a number")
+
+for (var i = 1; i <= 10; i++) {
+    var mul1 = mul * i;
+    if (mul === 5) {
+
+        document.write("<br>" + mul + " * " + i + " = " + mul1)
+        break
+    }
+    else {
+        document.write("<br>" + mul + " * " + i + " = " + mul1)
+    }
+}
+//  task 5
+var subject1 = +prompt("Enter a number")
+var subject2 = +prompt("Enter a number")
+var subject3 = +prompt("Enter a number")
+var obtainedMarks = subject1 + subject2 + subject3;
+var totalMarks = 300;
+var percentage = (obtainedMarks / totalMarks) * 100;
+document.write("English  " + subject1 + "<br>Urdu  " + subject2 + "<br>Math  " + subject3 + "<br>Obtained Marks " + obtainedMarks + "<br>Total Marks " + totalMarks + "<br>_________________" + "<br><br>Percentage " + percentage + "%")
+
+// Chapter 9-11
+// task 1
+var city = prompt("Enter a name of largest city in Pakistan", "Enter a city name")
+city = city.toLowerCase()
+if (city === "karachi") {
+    alert("Welcome to city of lights")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 2
+var gender = prompt("Write a gender", "Enter a gender")
+if (gender === "male") {
+    alert("Good Morning Sir")
+}
+else if (gender === "female") {
+    alert("Good Morning Ma'am")
+} else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+
+}
+//  task 3
+var color1 = prompt("Which signal color you have saw to Must Stop")
+color1 = color1.toLowerCase()
+var color2 = prompt("Which signal color you have saw to Ready to Move")
+color1 = color2.toLowerCase()
+var color3 = prompt("Which signal color you have saw to Move Now")
+color1 = color3.toLowerCase()
+if (color1 === "red") {
+    alert("Must Stop")
+}
+else if (color2 === "yellow") {
+    alert("Ready to move")
+}
+else if (color3 === "green") {
+    alert("Move now")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 4
+var fuelInCar = prompt("Enter remaining fuel in car (in litres)")
+if (fuelInCar < 0.25 && fuelInCar > 0) {
+    alert("Please refill the fuel in your car")
+}
+else if (fuelInCar >= 0.25 && fuelInCar <= 10) {
+    alert("Fuel in your car is probably fulled")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 5
+var a = 4;
+if (++a === 5) {
+    alert("given condition for variable a is true");
+}
+var b = 82;
+if (b++ === 83) {
+    alert("given condition for variable b is false");
+}
+var c = 12;
+if (++c < 14) {
+    alert("condition 3 is true");
+}
+var materialCost = 20000; var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) {
+    alert("The cost equals");
+}
+// task 6
+var marksObtained1 = +prompt("Enter the obtained marks");
+var totalMarks1 = 300;
+var percentage1 = (marksObtained1 / totalMarks1) * 100;
+if (marksObtained1 >= 80 && marksObtained1 <= 100) {
+    alert("<br>Total marks : " + totalMarks1 + "<br>Marks obtained : " + marksObtained1 + "<br>Percentage : " + percentage1 + "<br>Grade : A-one" + "<br>Remarks : Excellent")
+
+}
+else if (marksObtained1 >= 70 && marksObtained1 < 80) {
+    alert("<br>Total marks : " + totalMarks1 + "<br>Marks obtained : " + marksObtained1 + "<br>Percentage : " + percentage1 + "<br>Grade : A" + "<br>Remarks : Good")
+}
+else if (marksObtained1 >= 60 && marksObtained1 < 70) {
+    alert("<br>Total marks : " + totalMarks1 + "<br>Marks obtained : " + marksObtained1 + "<br>Percentage : " + percentage1 + "<br>Grade : B" + "<br>Remarks : You need to improve")
+}
+else if (marksObtained1 < 60) {
+    alert("<br>Total marks : " + totalMarks1 + "<br>Marks obtained : " + marksObtained1 + "<br>Percentage : " + percentage1 + "<br>Grade : Fail" + "<br>Remarks : Sorry")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 7
+var num = prompt("To check the number is divisible by 3")
+if (num % 3 === 0) {
+    alert("" + num + " is an Odd number")
+}
+else if (num % 3 !== 0) {
+    alert("" + num + " is not a Odd number")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+
+}
+// task 8
+var num1 = prompt("To check the number is even or odd")
+if (num1 % 2 === 0) {
+    alert("" + num + " is an Even number")
+
+}
+else if (num1 % 3 == 0) {
+    alert("" + num + " is an Odd number")
+}
+else {
+    alert("Please enter a correct number")
+}
+// task 9
+var temperature = +prompt("Enter the temperature in Karachi")
+if (temperature > 40 && temperature < 50) {
+    alert("It is too hot outside.")
+}
+else if (temperature > 30 && temperature <= 40) {
+    alert("The Weather today is Normal.")
+}
+else if (temperature > 20 && temperature <= 30) {
+    alert("Today’s Weather is cool.")
+}
+else if (temperature > 10 && temperature <= 20) {
+    alert("OMG! Today’s weather is so Cool.")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 10
+var firstNumber = +prompt("Enter a integer number")
+var secondNumber = +prompt("Enter another integer number")
+var operation = prompt("Enter a operation")
+if (operation === "+") {
+    alert(firstNumber + secondNumber)
+}
+else if (operation === "-") {
+    alert(firstNumber - secondNumber)
+}
+else if (operation === "*") {
+    alert(firstNumber * secondNumber)
+}
+else if (operation === "/") {
+    alert(firstNumber / secondNumber)
+}
+else if (operation === "%") {
+    alert(firstNumber % secondNumber)
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+
+// Chapter 12-13
+// task 1
+function Ascii() {
+    var ascii = "";
+    for (var i = 0, len = TextBox1.value.length; i < len; i++) {
+        ascii = ascii + TextBox1.value.charCodeAt(i);
+    }
+    TextBox2.value = ascii;
+}
+// task 2
+var value5 = +prompt("Enter the value");
+var value6 = +prompt("Enter the value");
+if (value5 > value6) {
+    alert(" " + value5 + " is the larger number than other value")
+}
+else if (value6 > value5) {
+    alert(" " + value6 + " is the larger number than other value")
+}
+else if (value5 === value6) {
+    alert("Both value are equal")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 3
+var user = +prompt("Enter a integer number")
+if (user > 0) {
+    alert("Number is positive")
+}
+else if (user < 0) {
+    alert("Number is negative")
+}
+else if (user === 0) {
+    alert("Number is zero")
+}
+else {
+    alert("Sorry; you can write wrong ,please tryagain!")
+}
+// task 4
+var vowels = prompt("Enter a vowel")
+if (vowels === "a" || vowels === "e" || vowels === "i" || vowels === "o" || vowels === "u") {
+    alert("" + vowels + ", yes it is a vowel")
+} else {
+    alert("" + vowels + ", no it is not a vowel")
+}
+// task 5
+var greeting; 
+var hour = 13; 
+if (hour < 18) {
+    greeting = "Good day";
+}
+else { 
+    greeting = "Good evening";
+}
+// task 6
+var time = prompt("Enter correct time");
+if (time >= 0000 && time < 1200){
+    alert("Good morning!") 
+}
+else if(time >= 1200 && time < 1700){
+    alert("Good afternoon!") 
+}   
+else if(time >= 1700 && time < 2100){
+    alert("Good evening!") 
+}
+else if(time >= 2100 && time <= 2359){
+    alert("Good night!") 
+}
+else{
+    alert("Sorry; you can write wrong ,please tryagain!")
+
+}
+// Chapter 14-16
+// task 1
+var myObj = []
+// task 2
+// var myJSON = JSON.parse(myObj)
+// task 3
+var myObj = ["arijah","yawar","rafia"]
+// task 4
+var myObj1 = [1,2,3]
+// task 5
+var myObj2 =[true, false]
+// task 6
+var myObj3 = ["arijah",1,"yawar",2,"rafia"]
+// task 7
+var myObj4 = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+myObj4 = document.write("<br>"+myObj4)
+// task 8
+var students = ["Micheal","John","Tony"]
+var obtainedMarks1 = +prompt("Enter number of 1st Students");
+var obtainedMarks2 = +prompt("Enter number of 2nd Students");
+var obtainedMarks3 = +prompt("Enter number of 3rd Students");
+var totalMarks2 = 500;
+var totalMarks3 =  ((obtainedMarks1+obtainedMarks2+obtainedMarks3)/totalMarks2) * 100;
+var totalMarks4 = document.write("Score of Micheal is "+obtainedMarks1+".Percentage: "+totalMarks3)
+var totalMarks5 = document.write("Score of John is "+obtainedMarks2+".Percentage: "+totalMarks3)
+var totalMarks6 = document.write("Score of Tony is "+obtainedMarks3+".Percentage: "+totalMarks3)
+// task 9
+var ScoresOfStudents = [320,230,480,120]
+var ScoresOfStudents1 = ScoresOfStudents.sort()
+document.write(ScoresOfStudents1)
+// task 10
+var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+var selectedCities = cities.slice(2,4)
+document.write("Cities list:<br>"+cities+"<br><br>Selected cities list:<br> "+selectedCities)
+// task 11
+var city1 = ["Karachi","Peshawar","Lahore"]
+var city2 = city1.unshift("Sawat","Larkana")
+var city3 = city1.shift()
+document.write(city2)
+document.write(city3)
+// task 12
+var city4 = city1.push("Sukkur","MirpurKhas")
+var city5 = city1.pop()
+document.write(city4)
+document.write(city5)
+
+// Chapter 17-20
+// task 1
+var arr = [[]];
+// task 2
+// var arr;
+// arr[0][0] = 0;
+// arr[0][1] = 1;
+// arr[0][2] = 2;
+// arr[0][3] = 3;
+// arr[1][0] = 1;
+// arr[1][1] = 0;
+// arr[1][2] = 1;
+// arr[1][3] = 1;
+// arr[2][0] = 2;
+// arr[2][1] = 1;
+// arr[2][2] = 0;
+// arr[2][3] = 1;
+// console.log(arr);
+// task 3
+var i;
+for (i = 1; i<=10; i++){
+    alert(i)
+}
+// task 4
+var value$1 = prompt("Enter a number to show its multiplication table")
+var value$2 = prompt("Enter a length of multiplication table")
+var a;
+for (var i = 1; i <= value$2.length; i++){
+    a = value$1 * i;
+    alert("" +value$1+ " * "+i+" = "+a)
+}
+// task 5
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+document.write(fruits)
+document.write("<br>Element at index 0 is "+fruits[0])
+document.write("<br>Element at index 1 is "+fruits[1])
+document.write("<br>Element at index 2 is "+fruits[2])
+document.write("<br>Element at index 3 is "+fruits[3])
+document.write("<br>Element at index 4 is "+fruits[4])
+// task 7
+var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+var C = A.indexOf() 
+var B = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'am?")
+if(A === B){
+    console.log(B+" is available at index "+C+" in our bakery")
+}
+else{
+    console.log("We are sorry."+A+" is not available in our bakery.")
+}
+// task 8
+var Data = [24, 53, 78, 91, 12] 
+var data1 = Data.max()
+console.log("Array items: "+Data+"The largest number is "+data1)
+// task 9
+var Data2 = [24, 53, 78, 91, 12] 
+var data2 = Data2.min()
+console.log("Array items: "+Data2+"The largest number is "+data2)
+// task 10
+for(var i=5; i<=100; i = i+5){
+    alert(i)
+}
+
